b820157a3de606031f0518e1f69a0515def969b4
GreatGonthar <gontharforeverrules@gmail.com>
2021-04-18 19:24:39 +1000

    добавили сегодняшнюю дату
    добавили сегодняшнюю дату в заголовке, и сделали чередования цветов строчек списка

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+let today = new Date();
+let dateOptions = {    
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',    
+  };
+let todayDateInDOM = document.querySelector(".today_date");
 let myButton = document.querySelector(".to_do_button");
 let myList = document.querySelector(".my_list");
 let message = myButton.onclick = (() => myList.innerHTML += "<li>" + (prompt("что сделать?", "ничего")) + "</li>");
-console.log (myList.textContent);
+
+//TODO обработать клики на li элементах
+let myLi = document.querySelectorAll("li");
+
+
+todayDateInDOM.textContent = today.toLocaleString("ru", dateOptions);
